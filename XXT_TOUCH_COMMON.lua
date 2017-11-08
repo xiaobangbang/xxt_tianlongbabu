76e1fb78811970ddc1b83a631fb155c16ce5b548
xiaobangbang <1196855008@qq.com>
2017-11-08 22:24:25 +0800

    xxt version first commit

---
+++ XXT_TOUCH_COMMON.lua
@@ -0,0 +1,212 @@
+
+function xxtmoveto(x1,y1,x2,y2,step)
+	touch.on(x1, y1):move(x2,y2):off() 
+end
+
+if TAB_ENV.LUA_VERSION == "TOUCH" then
+	open_app= runApp
+	front_app = frontAppBid
+	mmsleep = mSleep
+	init_screen = init
+	init_log = initLog
+	wt_log= wLog
+	close_log = closeLog
+	multi_col = multiColor
+	ltap = tap
+	move2 = moveTo
+	findcolor = findMultiColorInRegionFuzzy
+	ttoast = toast
+	unlock_device = unlockDevice
+	is_device_lock = deviceIsLock
+	getcolorr = getColor
+	set_screen_light = setBacklightLevel
+	get_screen_light = getBacklightLevel
+	keep_screen = keepScreen
+
+elseif TAB_ENV.LUA_VERSION == "XXT" then
+	open_app= app.run
+	front_app = app.front_bid
+	mmsleep = sys.msleep
+	init_screen = screen.init
+	wx_log= sys.log
+	multi_col = screen.is_colors
+	ltap = touch.tap
+	move2 =xxtmoveto
+	findcolor = screen.find_color
+	ttoast = sys.toast
+	unlock_device = device.unlock_screen
+	is_device_lock = device.is_screen_locked
+	getcolorr = screen.get_color
+	set_screen_light = device.set_brightness
+	get_screen_light = device.brightness
+	keep_screen = screen.keep
+	unkeep_screen = screen.unkeep
+end
+
+function f_keep_screen(flag)
+	if TAB_ENV.LUA_VERSION == "TOUCH" then 
+		if flag == true then
+			keep_screen(true)
+		else
+			keep_screen(false)
+		end
+	elseif TAB_ENV.LUA_VERSION == "XXT" then
+		if flag == true then
+			keep_screen()
+		else
+			unkeep_screen()
+		end
+	end
+end
+
+if TAB_ENV.LUA_VERSION == "TOUCH" then
+	init_log(log_file, 1)
+end	
+
+function wwlog(msg)
+	if TAB_ENV.LUA_VERSION == "TOUCH" then		
+		wt_log(log_file,msg)
+	elseif TAB_ENV.LUA_VERSION == "XXT" then
+		wx_log(msg)
+	end	
+	if TAB_ENV.DEBUG_MODE == true then
+		nLog(msg)
+	end
+end
+
+function f_is_device_locked()
+	local ret = false
+	if TOUCH_MODE == true then
+		if is_device_lock() ~= 0 then
+			ret = true	
+		end
+	elseif XXT_MODE == true then
+		ret = is_device_lock()
+	end	
+	return ret
+end
+
+function shake_screen()
+	local br = get_screen_light()
+	mmsleep(200)
+	for i = 1 ,30 do
+		set_screen_light(1)
+		mmsleep(200)
+		set_screen_light(0)
+		mmsleep(200)
+	end
+	set_screen_light(br)
+end
+
+
+function f_no_color_changed ()
+	local tab_luoxia ={
+		{ 1071,   50, 0xf0e9e9},
+		{ 1072,   58, 0xeae3e3},
+		{ 1079,   49, 0xdccaca},
+		{ 1080,   54, 0xefe7e7},
+		{ 1087,   54, 0xf5f0f0},
+		{ 1093,   59, 0xebe8e6},
+		{ 1100,   54, 0xf1ecec},
+		{ 1108,   59, 0xf3f2f1},
+		{ 1110,   52, 0xdcd2d2},
+		{ 1117,   53, 0xeceae9},
+	}
+
+	return function (v_over_time) 
+		local ret = true
+		local over_time = v_over_time or 1
+		over_time = over_time * 1000
+		mmsleep(over_time)
+		for k,v in  ipairs(tab_luoxia) do				
+			local c = getcolorr(v[1],   v[2])		
+			if c ~= v[3] then			
+				v[3] = c
+				ret = false
+			end
+		end
+		return ret
+	end
+end
+
+
+local tab_line = {
+	{  329,  202, 0x68351b},
+	{  440,  203, 0xc3976a},
+	{  329,  260, 0x724024},
+	{  444,  260, 0x643219},
+	{  332,  317, 0x7e4f30},
+	{  445,  316, 0x613118},
+	{  330,  373, 0x653319},
+	{  441,  373, 0x996844},
+	{  328,  428, 0xf3c992},
+	{  443,  428, 0x763c1d},
+}
+
+
+function get_line_xy(j)
+	local ret,x,y
+	for i,v in ipairs(tab_line) do
+		if i == j then
+			ret = true
+			x = v[1]	
+			y = v[2]			
+			break		
+		end
+	end	
+	return ret,x,y	
+end
+
+
+List ={}
+function List.new()
+	return {first=0,last = -1 }
+end
+
+function List.pushfirst(list,value)
+	local first = list.first -1
+	list.first = first
+	list[first] = value
+end
+
+function List.pushlast(list,value)
+	local last = list.last +1
+	list.last= last
+	list[last] = value
+end
+
+function List.popfirst(list)
+	local first = list.first
+	if first > list.last then error("list is empty") end
+	local value= list[first]
+	list[first] = nil
+	list.first= first +1
+	return value
+end
+
+function List.getfirst(list)
+	local first = list.first
+	if first > list.last then error("list is empty") end
+	local value= list[first]
+	--list[first] = nil
+	--list.first= first +1
+	return value
+end
+
+function List.poplast(list)
+	local last = list.last
+	if list.first > last then error("list is empty") end
+	local value = list[last]
+	list[last]= nil
+	list.last = last -1
+	return value
+end
+
+function getClickXY(color_arr)
+	local click_index = #color_arr
+	local click_xy = color_arr[click_index] 
+	local click_x = click_xy[1]
+	local click_y = click_xy[2]
+	return click_x,click_y
+	-- body
+end
---
+++ main.lua
@@ -0,0 +1,195 @@
+--[[
+2017-10-19
+QQ登陆
+王者荣耀
+--]]
+--require("TSLib")
+--require "req_tab1"
+--require "abc"
+require "tools"
+
+--iphone_path="/var/mobile/Media/TouchSprite/lua/"
+XXT_PHONE_PATH="/var/mobile/Media/1ferver/lua/scripts/"
+
+dofile(XXT_PHONE_PATH.."TAB_ENV.lua")
+dofile(XXT_PHONE_PATH.."VAR_SERIALIZE.lua")
+dofile(XXT_PHONE_PATH.."XXT_TOUCH_COMMON.lua")
+--dofile(iphone_path.."pushover.lua")
+
+function set_jue_se(...)
+	wwlog("298 判断是否是仓库号")
+	mmsleep(100)	
+
+	wwlog(tostring(VAR_LIST1.tianlei_flag))
+	VAR_LIST1.tianlei_flag = true
+	wwlog(tostring(VAR_LIST1.tianlei_flag))
+	VAR_LIST1.init_setup = true
+	fuli_flag = true
+	in_game = false
+
+	VAR_LIST1.bag_is_ready = true
+	wwlog("340 是仓库号,直接去交易")
+
+	-- body
+end
+
+
+function record_var1(o)
+	local out =assert(io.open(XXT_PHONE_PATH.."VAR_SERIALIZE.lua","w"))
+	out:write("VAR_LIST1 = ")
+	f_abc = function(o)
+		if type(o) == "number" then
+			out:write(o)
+		elseif type(o) == "string" then
+			out:write(string.format("%q",o) )		
+		elseif type(o) == "boolean" then
+			if o ==true then
+				out:write( "true")
+			else
+				out:write( "false")
+			end
+		elseif type(o) == "table" then		
+			out:write("{\n")
+			for k,v in pairs(o) do
+				out:write(" ",k,"=")
+				f_abc(v)
+				out:write(",\n")
+			end
+			out:write("}\n")
+		else
+			error("cannot serialize a "..type(o))
+		end
+	end
+
+	f_abc(o)
+	assert (out:close())
+
+end
+
+mmsleep(200)
+
+while (f_is_device_locked()) do
+	unlock_device()
+	mmsleep(1000)
+end
+ttoast("屏幕已解锁，脚本开始")
+
+init_screen(1)
+init("com.tencent.cyoutstl",1)
+
+--dofile(iphone_path.."main1.lua")
+mmsleep(1000)
+
+if front_app() == "com.tencent.xin" then
+		wwlog("74 微信程序ing...")
+	elseif  front_app() ~= "com.tencent.cyoutstl" then
+		mmsleep(200)
+		open_app("com.tencent.cyoutstl")
+		wwlog("game start")
+		--mmsleep(7000)
+		--ltap( 565,  332) --点击跳过动画
+		--wwlog("93 --点击跳过动画")
+		mmsleep(2000)
+		--before_game = 'Y'
+	end
+	
+
+if  isFrontApp("com.tencent.smoba") == 0 then
+	mmsleep(200)
+	runApp("com.tencent.smoba")
+	mmsleep(1000)
+end
+
+set_jue_se()
+record_var1(VAR_LIST1)
+
+co1 = coroutine.create( function() 
+		while true do
+			--for i = 1,2 do
+			ttoast("before yield")
+			coroutine.yield("sdfsdfsfdsdf")
+			mmsleep(3000)
+			ttoast("after yield")
+		end
+	end ) 
+
+function cor_pop_up1()
+	local a,b = coroutine.resume(co1)
+	if a then
+		ttoast(tostring(b))
+	end
+
+	return b
+end
+
+--pushmsg("title","msg11")
+
+--shake_screen()
+
+--cor_pop_up1()
+--mmsleep(3000)
+--cor_pop_up1()
+
+
+dofile(XXT_PHONE_PATH.."pick_color.lua")
+mmsleep(1000)
+
+list1= List.new()
+
+List.pushlast(list1,page_login_entrance.button_qq)
+List.pushlast(list1,page_login_auth_qq.button_login)
+
+
+function task_by_loop(list1)
+	-- body
+	while (true) do
+		for k,v in pairs (list1) do
+			--print(k)
+			if k ~= 'first' and k ~= 'last' then
+				nLog("k:"..k)
+				if multi_col(v) then
+					nLog("multi_col(v)")			
+					local click_x,click_y = getClickXY(v)	
+					tap(click_x,  click_y)
+				end 
+				--print_r(v)
+			end
+			mmsleep(1000)
+		end
+		mmsleep(1000)
+	end
+end
+
+function  getListSize(list1)
+	-- body
+	--[[
+	local size =0
+	for k,v in pairs (list1) do		
+		if k ~= 'first' and k ~= 'last' then
+			size = size +1
+		end
+	end
+	return size
+	--]]
+	return list1.last + 1
+end	
+
+function task_by_order(list1)
+	-- body	
+	--for i =1, getListSize(list1) do
+	while (true) do
+		-- body
+		local first_value = List.getfirst(list1)
+
+		local click_x,click_y = getClickXY(first_value)	
+		if multi_col(first_value) then
+			nLog("multi_col(first_value)")
+			List.popfirst(list1)			
+			tap(click_x,  click_y)
+		end
+		if getListSize(list1) == 0 then
+			break
+		end
+		mmsleep(1000)
+	end
+end
